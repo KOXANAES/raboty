d65388d7474bba0144b85492f13742c9e88493db
koxanaes <139272482+KOXANAES@users.noreply.github.com>
2023-09-26 17:48:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+function func1() {
+    document.getElementById("random-button");
+    let c=Math.floor(Math.random() * 22);
+    document.getElementById("random-name").innerHTML=c;
+    if (c==0) {
+        document.getElementById("random-name").innerHTML="Шавкунов";
+    }
+    if (c==1) {
+        document.getElementById("random-name").innerHTML="Абрамович";
+    }
+    if (c==2) {
+        document.getElementById("random-name").innerHTML="Буйко";
+    }
+    if (c==3) {
+        document.getElementById("random-name").innerHTML="Гавриленко";
+    }
+    if (c==4) {
+        document.getElementById("random-name").innerHTML="Жукенов";
+    }
+    if (c==5) {
+        document.getElementById("random-name").innerHTML="Зубович";
+    }
+    if (c==6) {
+        document.getElementById("random-name").innerHTML="Каленик";
+    }
+    if (c==7) {
+        document.getElementById("random-name").innerHTML="Кохановский";
+    }
+    if (c==8) {
+        document.getElementById("random-name").innerHTML="Кураленя";
+    }
+    if (c==9) {
+        document.getElementById("random-name").innerHTML="Лемнёв";
+    }
+    if (c==10) {
+        document.getElementById("random-name").innerHTML="Лукин";
+    }
+    if (c==11) {
+        document.getElementById("random-name").innerHTML="Листопад";
+    }
+    if (c==12) {
+        document.getElementById("random-name").innerHTML="Мацкевич";
+    }
+    if (c==13) {
+        document.getElementById("random-name").innerHTML="Муратов";
+    }
+    if (c==14) {
+        document.getElementById("random-name").innerHTML="Пашенцев";
+    }
+    if (c==15) {
+        document.getElementById("random-name").innerHTML="Папсуев";
+    }
+    if (c==16) {
+        document.getElementById("random-name").innerHTML="Сигатов";
+    }
+    if (c==17) {
+        document.getElementById("random-name").innerHTML="Симонов";
+    }
+    if (c==18) {
+        document.getElementById("random-name").innerHTML="Тараненко";
+    }
+    if (c==19) {
+        document.getElementById("random-name").innerHTML="Трифонов";
+    }
+    if (c==20) {
+        document.getElementById("random-name").innerHTML="Цыро";
+    }
+    if (c==21) {
+        document.getElementById("random-name").innerHTML="Четович";
+    }
+}
